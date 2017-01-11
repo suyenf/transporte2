5f85d0e82923b5278f665668405956cbe18c3acf
Su-yen Faudito <suyenfaudito@gmail.com>
2017-01-11 03:02:47 -0430

    plantilla
    depurar de codigo que no se use

--- application/views/base/cabeceras/menu_.php
+++ application/views/base/cabeceras/menu_.php
@@ -114,13 +114,11 @@
 <!-- LISTA VERTICAL -->
             <ul class="nav nav-list">
                 <li class="">
-                    
                     <a href="<?= base_url('index.php/transporte/Transporte/index')?>">
                     <!--<a href="transporte/home.php">-->
                         <i class="menu-icon fa fa-tachometer"></i>
                         <span class="menu-text"> Inicio </span>
                     </a>
-
                     <b class="arrow"></b>
                 </li>
 
@@ -286,9 +284,9 @@
             <i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
         </div>
          </div>
-
+<!--- REVISARRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR --->
         <div class="main-content">
-            <!--<div class="main-content-inner">-->
+            <div class="main-content-inner">
                 <div class="page-content"></div>
                 
                     <div class="ace-settings-container" id="ace-settings-container">
@@ -315,7 +313,7 @@
 
                             <div class="pull-left width-50">
                                 <div class="ace-settings-item">
-                                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-hover" autocomplete="off" />
+                                    <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-hover" autocomplete="off" </input>
                                     <label class="lbl" for="ace-settings-hover"> Submenú en Hover</label>
                                 </div>
 
@@ -332,52 +330,10 @@
                         </div><!-- /.ace-settings-box -->
                     </div>
                 <!-- /.ace-settings-container -->
-                <!--</div>-->
+                </div>
+        <!--</div>-->
                     
-                        <div class="col-xs-12">
-                            <!-- PAGE CONTENT BEGINS -->
-                            
-                            <div class="invisible" id="main-widget-container">
-                              <div class="row">
-                                
-
-                          <!--          <div class="col-xs-12 col-sm-6 widget-container-col" id="widget-container-col-2">
-                                        <div class="widget-box widget-color-blue" id="widget-box-2">
-                                            <div class="widget-header">
-                                                <h5 class="widget-title bigger lighter">
-                                                    <i class="ace-icon fa fa-table"></i>
-                                                    Tables & Colors
-                                                </h5>
-<!--
-                                                <div class="widget-toolbar widget-toolbar-light no-border">
-                                                    <select id="simple-colorpicker-1" class="hide">
-                                                        <option selected="" data-class="blue" value="#307ECC">#307ECC</option>
-                                                        <option data-class="blue2" value="#5090C1">#5090C1</option>
-                                                        <option data-class="blue3" value="#6379AA">#6379AA</option>
-                                                        <option data-class="green" value="#82AF6F">#82AF6F</option>
-                                                        <option data-class="green2" value="#2E8965">#2E8965</option>
-                                                        <option data-class="green3" value="#5FBC47">#5FBC47</option>
-                                                        <option data-class="red" value="#E2755F">#E2755F</option>
-                                                        <option data-class="red2" value="#E04141">#E04141</option>
-                                                        <option data-class="red3" value="#D15B47">#D15B47</option>
-                                                        <option data-class="orange" value="#FFC657">#FFC657</option>
-                                                        <option data-class="purple" value="#7E6EB0">#7E6EB0</option>
-                                                        <option data-class="pink" value="#CE6F9E">#CE6F9E</option>
-                                                        <option data-class="dark" value="#404040">#404040</option>
-                                                        <option data-class="grey" value="#848484">#848484</option>
-                                                        <option data-class="default" value="#EEE">#EEE</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                  --><!-- /.span -->
-                              </div><!-- /.row -->
-
-                            </div><!-- /.main-container-->
-                        </div>
-                         
+                      
                     
                         
     <!-- basic scripts -->
