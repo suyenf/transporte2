cb4be7c4dcc3608a0a05388e21740a2ae82b1814
Su-yen Faudito <suyenfaudito@gmail.com>
2017-01-15 22:44:34 -0430

    tabla modificada

--- application/views/transporte/list_productos.php
+++ application/views/transporte/list_productos.php
@@ -1,27 +1,61 @@
+<!--
+<table class = "table table-hover table-condensed">
+				<tr>
+					<th>#</th>
+					<th>Nombre</th>
+					<th>Codigo</th>
+					<th>Creado</th>
+					<th>Observacion</th>
+					<th>Activo</th>
+				</tr>
+				<?php if(isset($productos)){ ?>
+				<?php 
+					$i = 0;
+					foreach ($productos as $row){ 
 
-<div >
+					//Campo activo
+					if($row->activo)
+						$act = "SI";
+					else
+						$act = "NO";
+				?>
+					<?php print "<tr>
+					<td>".++$i."</td>
+					<td><a href='".base_url()."modificar_producto/".$row->id."'>".$row->nombre."</a></td>
+					<td>".$row->codigo."</td>
+					<td>".$row->creado."</td>
+					<td>".$row->observacion."</td>
+					<td>".$act."</td>
+					</tr>" ?> 
+				<?php }}else{ print "No hay datos que mostrar<br><br>";  } ?>
+			</table>
+-->
+
+<div>
+<!--<div class="row">-->
     <div class="col-xs-12">
         <div class="widget-header">
-            <h1 class="widget-title">Productos</h1><br>
+            <h1 class="widget-title">Productos</h1>
         </div>
 
- <a href="<?= base_url('index.php/transporte/Transporte/crear_producto')?>">
-	<br><button class="btn btn-lg btn-success">
-		<i class="ace-icon fa fa-check"></i>Agregar producto
-	</button>
-</a>
         <div class="clearfix">
             <div class="pull-right tableTools-container"></div>
         </div>
-        <div class="table-header">
+        <div class="table-header" margin= "auto">
             Resultados para  "Últimos dominios registrados"
+            <a href="<?= base_url('index.php/transporte/Transporte/crear_producto') ?>"
+               <button class="btn btn-lg btn-success" margin= "auto">
+                    <i class="ace-icon fa fa-check">Agregar producto</i>
+                </button>
+            </a>
+        
         </div>
 
         <!-- div.table-responsive -->
 
         <!-- div.dataTables_borderWrap -->
         <div>
-            <table id="dynamic-table" class="table table-striped table-bordered table-hover">
+            <table id="dynamic-table" class="table table-striped table-bordered table-hover dataTable" style="font-size: 85%" margin= "auto">
                 <thead>
                     <tr>
                         
@@ -40,7 +74,9 @@
                             <i class="ace-icon fa fa-clock-o bigger-110 hidden-480"></i>
                             Creado
                         </th>
+                        <!--<th class="hidden-480">Observación</th>-->
                         <th class="hidden-480">Estado</th>
+                        <th colspan=""> Opciones</th>
 
                         <th></th>
                     </tr>
@@ -70,32 +106,19 @@
                         <!--</td>-->
 
                          <td> <?php echo ++$i; ?></td>
-                <a href="<?= base_url("/index.php/transporte/transporte/modificar_producto/{$row->id}{$row->nombre}") ?>"></a>
+                         <td> <?php echo $row->nombre; ?></td>
                          <td> <?php echo $row->codigo; ?></td>
                          <td> <?php echo $row->creado; ?> </td>
-                         <td> <?php echo $row->observacion; ?> </td>
+                         <!--<td> <?php echo $row->observacion; ?> </td>-->
                          <td> <?php echo $act; ?> </td>
                                                  
-                       
+                        
 					
 				<?php // }}else{ print "No hay datos que mostrar<br><br>";  } ?>
-			<!--</table>-->
-                        
-                        
-                        
-<!--                        <td>
-                            <a href="#">app.com</a>
-                        </td>
-                        <td>$45</td>
-                        <td class="hidden-480">3,330</td>
-                        <td>Feb 12</td>
-
-                        <td class="hidden-480">
-                            <span class="label label-sm label-warning">Expiring</span>
-                        </td>-->
+			
 
                         <td>
-                            <div class="hidden-sm hidden-xs action-buttons">
+                            <div class="hidden-sm hidden-xs action-buttons"margin= "auto">
                                 <a class="blue" href="#">
                                     <i class="ace-icon fa fa-search-plus bigger-130"></i>
                                 </a>
@@ -109,7 +132,7 @@
                                 </a>
                             </div>
 
-                            <div class="hidden-md hidden-lg">
+                            <div class="hidden-md hidden-lg" margin= "auto">
                                 <div class="inline pos-rel">
                                     <button class="btn btn-minier btn-yellow dropdown-toggle" data-toggle="dropdown" data-position="auto">
                                         <i class="ace-icon fa fa-caret-down icon-only bigger-120"></i>
@@ -1692,117 +1715,7 @@
     </div>
 </div>
 
-<div id="modal-table" class="modal fade" tabindex="-1">
-    <div class="modal-dialog">
-        <div class="modal-content">
-<!--            <div class="modal-header no-padding">
-                <div class="table-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-                        <span class="white">&times;</span>
-                    </button>
-                    Results for "Latest Registered Domains
-                </div>
-            </div>-->
-<!--
-            <div class="modal-body no-padding">
-                <table class="table table-striped table-bordered table-hover no-margin-bottom no-border-top">
-                    <thead>
-                        <tr>
-                            <th>Domain</th>
-                            <th>Price</th>
-                            <th>Clicks</th>
 
-                            <th>
-                                <i class="ace-icon fa fa-clock-o bigger-110"></i>
-                                Update
-                            </th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        <tr>
-                            <td>
-                                <a href="#">ace.com</a>
-                            </td>
-                            <td>$45</td>
-                            <td>3,330</td>
-                            <td>Feb 12</td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <a href="#">base.com</a>
-                            </td>
-                            <td>$35</td>
-                            <td>2,595</td>
-                            <td>Feb 18</td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <a href="#">max.com</a>
-                            </td>
-                            <td>$60</td>
-                            <td>4,400</td>
-                            <td>Mar 11</td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <a href="#">best.com</a>
-                            </td>
-                            <td>$75</td>
-                            <td>6,500</td>
-                            <td>Apr 03</td>
-                        </tr>
-
-                        <tr>
-                            <td>
-                                <a href="#">pro.com</a>
-                            </td>
-                            <td>$55</td>
-                            <td>4,250</td>
-                            <td>Jan 21</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
--->
-            <div class="modal-footer no-margin-top">
-                <button class="btn btn-sm btn-danger pull-left" data-dismiss="modal">
-                    <i class="ace-icon fa fa-times"></i>
-                    Close
-                </button>
-
-      <!--          <ul class="pagination pull-right no-margin">
-                    <li class="prev disabled">
-                        <a href="#">
-                            <i class="ace-icon fa fa-angle-double-left"></i>
-                        </a>
-                    </li>
-
-                    <li class="active">
-                        <a href="#">1</a>
-                    </li>
-
-                    <li>
-                        <a href="#">2</a>
-                    </li>
-
-                    <li>
-                        <a href="#">3</a>
-                    </li>
-
-                    <li class="next">
-                        <a href="#">
-                            <i class="ace-icon fa fa-angle-double-right"></i>
-                        </a>
-                    </li>
-                </ul>-->
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div>
 
 <!-- PAGE CONTENT ENDS -->
 
